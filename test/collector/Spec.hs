93e88a173cf5aa5f1cb23114cce06b5e7fbb3209
Mads Rolsdorph <m.rolsdorph@gmail.com>
2021-08-18 14:37:43 +0200

    Add more tests for SimpleSource collection
    Wordy, but covers quite a few scenarios!

--- test/collector/Spec.hs
+++ test/collector/Spec.hs
@@ -4,16 +4,43 @@
 
 import Data.Aeson (Value (Bool, Number, String), encode, object)
 import qualified Data.ByteString.Lazy as LB
+import Data.Either (isRight)
 import Data.Functor.Identity
 import Data.List (isInfixOf)
 import Data.Time.Calendar (Day (ModifiedJulianDay))
 import Data.Time.Clock (UTCTime (..), secondsToDiffTime)
 import qualified SimpleSource as SS
-import Test.Hspec (Spec, describe, hspec, it, shouldSatisfy)
+import Test.Hspec (Spec, describe, hspec, it, shouldBe, shouldSatisfy)
 import Types
 
 main :: IO ()
 main = hspec spec
+
+newtype FailingStack a = FailingStack (Identity a)
+  deriving (Functor, Applicative, Monad)
+
+runFailingStack :: FailingStack a -> a
+runFailingStack (FailingStack (Identity a)) = a
+
+instance SS.HasHttp FailingStack where
+  simpleGet _ _ = return $ Left "HTTP error"
+
+instance SS.HasLogger FailingStack where
+  infoLog = undefined
+  errorLog = undefined
+
+newtype InvalidJsonStack a = InvalidJsonStack (Identity a)
+  deriving (Functor, Applicative, Monad)
+
+runInvalidJsonStack :: InvalidJsonStack a -> a
+runInvalidJsonStack (InvalidJsonStack (Identity a)) = a
+
+instance SS.HasHttp InvalidJsonStack where
+  simpleGet _ _ = return $ Right invalidResponse
+
+instance SS.HasLogger InvalidJsonStack where
+  infoLog = undefined
+  errorLog = undefined
 
 newtype SuccessStack a = SuccessStack (Identity a)
   deriving (Functor, Applicative, Monad)
@@ -35,14 +62,37 @@ spec = do
       let res = runSuccessStack $ SS.collect (testSource {url = "MALFORMED"})
       res `shouldSatisfy` isErrorContaining "collection URL"
 
+    it "Returns an error when the JSON fetch fails" $ do
+      let res = runFailingStack $ SS.collect testSource
+      res `shouldSatisfy` isErrorContaining "HTTP error"
+
     it "Returns an error when encountering malformed JSON" $ do
-      putStrLn "TODO"
+      let res = runInvalidJsonStack $ SS.collect testSource
+      res `shouldSatisfy` isErrorContaining "not a valid json value"
 
     it "Returns an error if the source does not have an ID" $ do
-      putStrLn "TODO"
+      let res = runSuccessStack $ SS.collect testSource {genericSourceId = Nothing}
+      res `shouldSatisfy` isErrorContaining "Source ID missing"
 
     it "Extracts tags and fields from the result JSON" $ do
-      putStrLn "TODO"
+      let res = runSuccessStack $ SS.collect testSource
+      res `shouldSatisfy` isRight
+      case res of
+        (Left _) -> fail "Should never happen"
+        (Right collectedSource) -> do
+          sourceId collectedSource `shouldBe` testSourceId
+          sourceOwnerId collectedSource `shouldBe` shallowOwnerId testSource
+          datakey collectedSource `shouldBe` genericDataKey testSource
+
+          datapoints collectedSource
+            `shouldBe` [ DataPoint
+                           { tags = [("mappedKey1", IntValue 1337)],
+                             fields =
+                               [ ("mappedKey2", StringValue "what"),
+                                 ("mappedKey3", BoolValue False)
+                               ]
+                           }
+                       ]
 
 isErrorContaining :: String -> Either String a -> Bool
 isErrorContaining desired (Left actual) = desired `isInfixOf` actual
@@ -54,18 +104,24 @@ someTime = UTCTime (ModifiedJulianDay 0) (secondsToDiffTime 0)
 noopLogger :: String -> IO ()
 noopLogger = const (return ())
 
+testSourceId :: String
+testSourceId = "some-id"
+
 testSource :: SimpleShallowJsonSource
 testSource =
   SimpleShallowJsonSource
-    { genericSourceId = Just "some-id",
+    { genericSourceId = Just testSourceId,
       genericDataKey = "some-key",
       shallowOwnerId = "1234",
       shallowCreatedAt = someTime,
       url = "https://localhost:1337/source.json",
       authHeader = "auth here",
-      tagMappings = [],
-      fieldMappings = []
+      tagMappings = [("key1", "mappedKey1")],
+      fieldMappings = [("key2", "mappedKey2"), ("key3", "mappedKey3")]
     }
+
+invalidResponse :: LB.ByteString
+invalidResponse = "asd[[[]]{}"
 
 validResponse :: LB.ByteString
 validResponse = encode testResponse
