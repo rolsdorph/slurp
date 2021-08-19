848387f16952a1eea8fe5a5551c4d3bc6baf61a6
Mads Rolsdorph <m.rolsdorph@gmail.com>
2021-08-19 12:30:27 +0200

    Move notifier to read queue config from a Reader

--- notifier/Main.hs
+++ notifier/Main.hs
@@ -3,13 +3,18 @@
 module Main where
 
 import qualified Auth
+import Control.Monad.Reader (ReaderT, ask, liftIO, runReaderT)
 import qualified Notifier
 import Secrets
 import System.Log.Logger (emergencyM)
+import Types (QueueConfig)
 
 main :: IO ()
 main = do
   maybeQueueConfig <- readUserNotificationQueueConfig
   case maybeQueueConfig of
-    (Just queueConfig) -> Notifier.createConsumerRegistry queueConfig >>= Notifier.run Auth.verifyToken
+    (Just queueConfig) -> runReaderT app queueConfig
     _ -> emergencyM Notifier.loggerName "Notification queue config not found, refusing to start"
+
+app :: ReaderT QueueConfig IO ()
+app = ask >>= liftIO . Notifier.createConsumerRegistry >>= liftIO . Notifier.run Auth.verifyToken
